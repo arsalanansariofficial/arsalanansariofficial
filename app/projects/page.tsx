d10266129e8a0cd0e1a4f154dab98e233f875c66
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-04-14 16:19:13 +0530

    Add projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="grow content-center">
+      <h1 className="text-center text-3xl font-bold">Projects Page</h1>
+    </main>
+  );
+}
