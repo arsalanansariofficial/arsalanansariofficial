6a07ad26dd9f5ec99bf184e706677c4962016fb2
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-04-15 15:42:24 +0530

    Update header and footer components

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 
+import data from '@/data.json';
 import MenuToggle from '@/components/menu-toggle';
 import ThemeToggle from '@/components/theme-toggle';
 
 export default function Header() {
   return (
-    <header className="bg-background/75 fixed inset-x-0 top-0 z-10 px-8 py-4 backdrop-blur-sm">
-      <nav className="container mx-auto flex max-w-3xl items-center justify-between">
+    <header className="bg-background/75 fixed inset-x-0 top-0 z-10 backdrop-blur-sm">
+      <nav className="container mx-auto flex max-w-3xl items-center justify-between px-8 py-4">
         <Link href="/" className="font-serif text-2xl font-bold">
           AA
         </Link>
@@ -19,6 +20,9 @@ export default function Header() {
           </li>
           <li className="hover:text-foreground text-muted-foreground transition-colors">
             <Link href="/contact">Contact</Link>
+          </li>
+          <li className="hover:text-foreground text-muted-foreground transition-colors">
+            <a href={data.social.resume}>Resume</a>
           </li>
         </ul>
         <ThemeToggle />
