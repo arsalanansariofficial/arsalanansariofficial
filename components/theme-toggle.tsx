6cfe131c23eeb4346a901488b29c6bdce4bbfdcc
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-04-15 12:29:32 +0530

    Add theme-provider and theme-toggle components

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import * as React from 'react';
+import { useTheme } from 'next-themes';
+import { Moon, Sun } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      size="icon"
+      variant="ghost"
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-0 -rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:rotate-90" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
